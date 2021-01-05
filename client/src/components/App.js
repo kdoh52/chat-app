270613cb3cf7968432a7344e0f27d082af31a4a3
kdoh52 <kdoh52@gmail.com>
2021-01-05 15:19:30 -0800

    new conversations modal, context

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -5,6 +5,8 @@ import useLocalStorage from '../hooks/useLocalStorage';
 import Login from './Login'
 import Dashboard from './Dashboard'
 import { ContactsProvider } from '../contexts/ContactsProvider';
+import { ConversationsProvider } from '../contexts/ConversationsProvider';
+
 
 
 function App() {
@@ -13,7 +15,9 @@ function App() {
 
   const dashboard = (
     <ContactsProvider>
-      <Dashboard id={id} />
+      <ConversationsProvider>
+        <Dashboard id={id} />
+      </ConversationsProvider>
     </ContactsProvider>
   )
 
