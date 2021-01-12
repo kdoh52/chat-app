30ed55e46d0b544fe43d8895abbb037f2981265c
kdoh52 <kdoh52@gmail.com>
2021-01-11 17:22:31 -0800

    readme update

--- README.md
+++ README.md
@@ -1,1 +1,29 @@
-# chat-app
+# Chat App
+
+This is a simple chat web application created using Socket.io and React. It allows a user to sign in and communicate with others remotely using unique ID codes. Users can save contacts and create any combination of group chats with said people. Interface displays messages according to sender and recipients.
+
+This application was created with the help of <a href='https://github.com/WebDevSimplified'>WebDevSimplified</a>
+
+## Screenshots
+<img src="chat-app.png" alt="chat app screenshot">
+   
+## Deployed Application
+* TBA
+
+## Technologies
+* HTML
+* CSS
+* Javascript
+* Node.js
+* React
+* React-dom
+* Socket.io
+* UUID
+* Bootstrap
+* Heroku
+  
+## Portfolio
+<a href="https://kdoh52.github.io/" target="_blank">Updated portfolio</a>
+
+## Contact
+* kdoh52@gmail.com
--- client/src/components/OpenConversation.js
+++ client/src/components/OpenConversation.js
@@ -35,7 +35,7 @@ export default function OpenConversation() {
                     {selectedConversation.messages.map((message, index) => {
                         const lastMessage = selectedConversation.messages.length - 1 === index
                         return (
-                            <div ref={lastMessage ? setRef : null} key={index} className={`my-1 d-flex flex-column ${message.fromMe ? 'align-self-end' : ''}`}>
+                            <div ref={lastMessage ? setRef : null} key={index} className={`my-1 d-flex flex-column ${message.fromMe ? 'align-self-end align-items-end' : 'align-items-start'}`}>
                                 <div className={`rounded px-2 py-1 ${message.fromMe ? 'bg-primary text-white' : 'border'}`}>
                                     {message.text}
                                 </div>
