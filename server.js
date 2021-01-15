0dd2baa98dc6cfa0be9107dfd8ec88d2e28abe2a
kdoh52 <kdoh52@gmail.com>
2021-01-14 17:05:54 -0800

    express.static

--- server.js
+++ server.js
@@ -4,6 +4,9 @@ const app = express()
 const server = require('http').Server(app)
 const io = require('socket.io')(server)
 // app.use( express.static(__dirname + '/../../build'))
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static("client/build"));
+}
 
 io.on('connection', socket => {
     const id = socket.handshake.query.id
