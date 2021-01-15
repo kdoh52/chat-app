d2b13a46dd8e0f51a6fe94a1fefdfeebf91fa440
kdoh52 <kdoh52@gmail.com>
2021-01-14 17:19:05 -0800

    cors

--- server.js
+++ server.js
@@ -2,11 +2,18 @@
 const express = require('express')
 const app = express()
 const server = require('http').Server(app)
-const io = require('socket.io')(server)
+// const io = require('socket.io')(server)
+const io = require("socket.io")(server, {
+    cors: {
+      origin: process.env.SOCKET_URL || "http://localhost:5000",
+      //ws: "//the-coders-corner.herokuapp.com/socket.io/?EIO=4&transport=websocket",
+      methods: ["GET", "POST"],
+    },
+});
 // app.use( express.static(__dirname + '/../../build'))
-if (process.env.NODE_ENV === "production") {
-    app.use(express.static("client/build"));
-}
+// if (process.env.NODE_ENV === "production") {
+//     app.use(express.static("client/build"));
+// }
 
 io.on('connection', socket => {
     const id = socket.handshake.query.id
